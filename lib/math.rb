ed0929738088baebd0e3e783813136bfd21fc385
Kevin Glasgow <kglas001@gmail.com>
2020-09-02 23:06:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,15 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
+  difference
 end
 
 def division(num1, num2)
-
+  qu
 end
 
 def multiplication(num1, num2)
